0c85c8fb0f4272cf60a18ed5c1359accb6e7e196
Dylan <dylanw0lv@gmail.com>
2023-04-15 03:18:53 -0400

    fix weather

--- js/weather.js
+++ js/weather.js
@@ -1,38 +1,29 @@
 
 const fetchWeather = async () => {
-    const errorMSG = 
-`An error occured. This command may not work with an ad blocker enabled.`;
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+        let lon = pos.coords.longitude;
+        let lat = pos.coords.latitude;
 
-    const response = await fetch("http://ip-api.com/json");
-
-    if (!response.ok) {
-        typeWriterEffect(errorMSG, ".output", 10);
-    }
-
-    const result = await response.json();
-    let lat = result.lat;
-    let lon = result.lon;
-    let city = result.city;
-
-    let url = `https://1dbtfjoli1.execute-api.us-east-1.amazonaws.com/?lat=37&lon=-79`;
+        let url = `https://1dbtfjoli1.execute-api.us-east-1.amazonaws.com/?lat=${lat}&lon=${lon}`;
     
-    let requestWeather = await fetch(url);
-    let weatherData = await requestWeather.json();
+        let requestWeather = await fetch(url);
+        let weatherData = await requestWeather.json();
 
-    console.log(weatherData)
+        console.log(weatherData)
 
-    let temp = weatherData.main.temp;
-    let description = weatherData.weather[0].description;
-    let humidity = weatherData.main.humidity;
-    let windSpeed = weatherData.wind.speed;
-    let feelsLike = weatherData.main.feels_like;
-    let message =
+        let temp = weatherData.main.temp;
+        let description = weatherData.weather[0].description;
+        let humidity = weatherData.main.humidity;
+        let windSpeed = weatherData.wind.speed;
+        let feelsLike = weatherData.main.feels_like;
+        let message =
 `The current temperature in ${city} is ${temp}°F.
 It feels like ${feelsLike}°F.
 Description: ${description}
 Humidity: ${humidity}%
 Wind Speed: ${windSpeed} mph`;
 
-    typeWriterEffect(message, ".output", 10);
+        typeWriterEffect(message, ".output", 10);
+    });
 
 }
